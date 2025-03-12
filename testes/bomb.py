df4a7ad180471a211999a50808455991e4f9e667
Seu Nome <seu-email@exemplo.com>
2025-03-12 09:35:22 -0300

    python.testes

---
+++ testes/bomb.py
@@ -0,0 +1,11 @@
+import time
+
+numero = int(input("Digite um número para iniciar a contagem regressiva: "))
+tempo_espera = float(input("Digite o tempo de espera (em segundos) entre cada número: "))
+
+while numero >= 0:
+    print(numero)
+    numero -= 1
+    time.sleep(tempo_espera)  
+
+print("BUM!")
---
+++ testes/temp.py
@@ -0,0 +1,11 @@
+
+temperatura = float(input("Digite a temperatura: "))  
+
+if temperatura > 30:
+    print("Está quente!")
+elif temperatura > 20:
+    print("Está agradável.")
+elif temperatura > 10:
+    print("Está fresco.")
+else:
+    print("Está frio!")
